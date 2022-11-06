878955ab8acbf577a5b89a534734358c804feed6
BestProfessionals <113937008+BestProfessionals@users.noreply.github.com>
2022-11-06 22:15:44 +0430

    Update awm.py

--- awm.py
+++ awm.py
@@ -14,8 +14,8 @@ else:
 if 'aarch' in arch:
     arch = 'aarch'
     print('\033[1;32m\nCongratulatings! Your Deviec Support This Tools')
-    import awm
-    awm.menu()
+    import aw
+    aw.menu()
 else:exit('\033[1;31m Sorry System or device not supported ')
     
 
